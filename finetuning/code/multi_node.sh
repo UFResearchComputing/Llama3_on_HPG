2759a4be24e5d3b784237e8fd8092f4a873c598b
Qian Zhao <zhao.qian@ufl.edu>
2024-05-09 21:13:23 -0400

    Update

--- finetuning/code/multi_node.sh
+++ finetuning/code/multi_node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=llama2-2nodes8gpus
+#SBATCH --job-name=llama3-2nodes8gpus
 #SBATCH --mail-type=ALL               # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=zhao.qian@ufl.edu # Where to send mail
 #SBATCH --ntasks=2
@@ -17,12 +17,12 @@
 
 module load nlp/1.3
 
-cd /data/ai/tutorial/Llama2_on_HPG/finetuning
+cd /data/ai/tutorial/Llama3_on_HPG/finetuning
 
 # Running the torchrun command
 torchrun --nproc_per_node=4 \
               finetuning.py --enable_fsdp --use_peft --peft_method lora \
-              --model_name /data/ai/models/nlp/llama/models_llama2/llama-2-13b-hf \
-              --output_dir /data/ai/tutorial/Llama2_on_HPG/finetuning/models/2nodes8gpus
+              --model_name /data/ai/models/nlp/llama/models_llama3/Meta-Llama-3-70B-hf \
+              --output_dir /data/ai/tutorial/Llama3_on_HPG/finetuning/models/2nodes8gpus
 
 
